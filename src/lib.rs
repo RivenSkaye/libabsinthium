797cf1c49f4d4eb4ca5f198955c9f201a40528f0
Riven Skaye <riven@tae.moe>
2025-02-25 17:24:23 +0100

    oops forgot to commit and push

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,51 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{borrow::Cow, marker::PhantomData};
+
+/// A trait to describe pretty bare metadata, inspired by extended m3u, which is the most common format.
+pub trait TrackMetadata {
+    /// Produce the title
+    fn title(&self) -> Cow<str>;
+    /// Produce the track length, if present
+    fn length(&self) -> Option<u32>;
+    /// Produce all info, formatted
+    fn info(&self) -> Cow<str>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Bare track information for a playlist.
+///
+/// If not explicitly available, this info can always be inferred.
+pub trait Track<T: TrackMetadata> {
+    /// Get the number of the track. Or its position in the playlist, if not specified
+    fn track_num(&self) -> u32;
+    /// Get the filename or URI this track points to
+    fn filename(&self) -> Cow<str>;
+    /// If present, get the metadata object
+    fn metadata(&self) -> T;
+    /// Overwrite the metadata object
+    fn write_metadata(&mut self, metadata: T);
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait PlaylistInfo {
+    /// If the playlist metadata defines a title or name for the playlist, produce it.
+    fn title(&self) -> Option<Cow<str>>;
+    /// Provide the filename or URI this playlist is found
+    fn filename(&self) -> Cow<str>;
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct Playlist<P: PlaylistInfo, _T: TrackMetadata, E: Track<_T>> {
+    entries: Vec<E>,
+    info: P,
+    phantom: PhantomData<_T>,
+}
+
+impl<P: PlaylistInfo + Default, T: TrackMetadata + Default, E: Track<T> + Default>
+    Playlist<P, T, E>
+{
+    pub fn new(info: P, entries: Vec<E>) -> Self {
+        Self {
+            entries,
+            info,
+            phantom: PhantomData,
+        }
     }
 }
