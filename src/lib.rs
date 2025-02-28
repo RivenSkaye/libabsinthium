1c0ffeedcd69988aa6bad4ac9509317a01f52d12
Riven Skaye <riven@tae.moe>
2025-02-28 22:14:20 +0100

    Flesh out more of the API, require types to be `Clone` (`Copy` is fine too)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Cow, cell::UnsafeCell, marker::PhantomData, path::PathBuf};
+use std::{borrow::Cow, cell::RefCell, marker::PhantomData, path::PathBuf};
 
 /// A trait to describe pretty bare metadata, inspired by extended m3u, which is the most common format.
 pub trait EntryMetadata {
@@ -13,13 +13,13 @@ pub trait EntryMetadata {
 /// Bare entry information for a playlist.
 ///
 /// If not explicitly available, this info can always be inferred.
-pub trait Entry<M: EntryMetadata + Default> {
+pub trait Entry<M: EntryMetadata + Default + Clone> {
     /// Get the number of the entry. Or its position in the playlist, if not specified
     fn entry_num(&self) -> u32;
     /// Get the filename or URI this entry points to
     fn filename(&self) -> Cow<str>;
     /// If present, get the metadata object
-    fn metadata(&self) -> M;
+    fn metadata(&self) -> &M;
     /// Overwrite the metadata object
     fn write_metadata(&mut self, metadata: M);
 }
@@ -32,9 +32,9 @@ pub trait PlaylistInfo {
 }
 
 pub trait PlalistFormat<
-    P: PlaylistInfo + Default,
-    M: EntryMetadata + Default,
-    E: Entry<M> + Default,
+    P: PlaylistInfo + Default + Clone,
+    M: EntryMetadata + Default + Clone,
+    E: Entry<M> + Default + Clone,
 >
 {
     /// Read a file or URI into a playlist
@@ -47,27 +47,44 @@ pub trait PlalistFormat<
     fn parse_playlist_info<S: AsRef<str>>(text: impl Into<S>) -> P;
 }
 
-#[derive(Debug, Default)]
-pub struct Playlist<P: PlaylistInfo + Default, T: EntryMetadata + Default, E: Entry<T> + Default> {
+#[derive(Default)]
+pub struct Playlist<
+    P: PlaylistInfo + Default + Clone,
+    T: EntryMetadata + Default + Clone,
+    E: Entry<T> + Default + Clone,
+> {
     /// Playlist entries, kept in an [`UnsafeCell`] for interior mutability purposes
-    entries: UnsafeCell<Vec<E>>,
+    entries: RefCell<Vec<E>>,
     /// Playlist info, kept jn an [`UnsafeCell`] for mutability purposes
-    info: UnsafeCell<P>,
+    info: RefCell<P>,
+    #[doc(hidden)]
     phantom: PhantomData<T>,
 }
 
-impl<P: PlaylistInfo + Default, T: EntryMetadata + Default, E: Entry<T> + Default>
-    Playlist<P, T, E>
+impl<
+        P: PlaylistInfo + Default + Clone,
+        T: EntryMetadata + Default + Clone,
+        E: Entry<T> + Default + Clone,
+    > Playlist<P, T, E>
 {
+    /// Creates a playlist from a block of metadata and a Vec of entries
     pub fn from_parts(info: P, entries: Vec<E>) -> Self {
         Self {
-            entries: UnsafeCell::new(entries),
-            info: UnsafeCell::from(info),
+            entries: RefCell::new(entries),
+            info: RefCell::new(info),
             phantom: PhantomData,
         }
     }
 
-    fn add_entry(&self, entry: E) {
-        unsafe { self.entries.get().as_mut().unwrap() }.push(entry);
+    pub fn add_entry(&self, entry: E) {
+        self.entries.borrow_mut().push(entry);
+    }
+
+    pub fn get_metadata(&self) -> P {
+        self.info.borrow().clone()
+    }
+
+    pub fn count(&self) -> usize {
+        self.entries.borrow().len()
     }
 }
